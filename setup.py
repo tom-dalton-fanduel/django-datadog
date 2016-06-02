e06fbe6d6c5ef655ddcc068bad4ac93ddf2ccf97
Tom Dalton <tom.dalton@fanduel.com>
2016-06-02 11:31:19 +0100

    Make `setup.py check` run successfully

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     include_package_data = True,
     license = 'BSD',
     description = 'Simple Django middleware for submitting timings and exceptions to Datadog.',
-    long_description='See https://github.com/tom-dalton-fanduel/fdjangodog',
+    url='https://github.com/tom-dalton-fanduel/fdjangodog',
     author='Tom Dalton',
     author_email='tom.dalton@fanduel.com',
     install_requires=[
